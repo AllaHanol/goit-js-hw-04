0bf2d6e92d772d537da7f24557bc7de42d8fe89a
AllaHanol <ganol2014@gmail.com>
2024-04-24 21:30:48 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+
+ function isEnoughCapacity(products, containerSize) {
+   const quantityArr = Object.values(products); 
+   let quantity = 0;
+  for (let i = 0; i < quantityArr.length; i++) {
+     quantity += quantityArr[i];
+  }
+   const result = quantity <= containerSize ? true : false;
+
+  return result;
+}
+
+console.dir(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.dir(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.dir(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.dir(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
